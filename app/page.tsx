091b42a0ba461d528910b9c3af262fef3bd90e8e
najafsikander <najafsikander@yahoo.com>
2024-08-13 17:45:45 +0400

    Completed Guest Component along with Clerk Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
-const HomePage = () => {
+import Guest from "@/components/Guest";
+import { checkUser } from "@/lib/checkUser";
+const HomePage = async () => {
+
+  const user = await checkUser();
+
+  if(!user) return <Guest/>;
+
   return ( <main>
-    <h1>Expense Tracker</h1>
+    <h1>Welcome, {user.name}</h1>
   </main> );
 }
  
